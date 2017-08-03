2f771b125eda50e53de777c1714d0d0b5e12dd94
mlibai <mlibai@163.com>
2017-08-03 20:31:37 +0800

    OPT

--- OMKit/Classes/AppExport/OMAppNetworkExport.h
+++ OMKit/Classes/AppExport/OMAppNetworkExport.h
@@ -13,10 +13,26 @@
 typedef NSString *OMAppNetworkType NS_STRING_ENUM NS_SWIFT_NAME(AppNetworkType);
 FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkTypeNone;
 FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkTypeWiFi NS_SWIFT_NAME(WiFi);
-FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType2G;
-FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType3G;
-FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType4G;
-FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkTypeUnkonwn;
+FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType2G NS_SWIFT_NAME(WWan2G);
+FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType3G NS_SWIFT_NAME(WWan3G);
+FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkType4G NS_SWIFT_NAME(WWan4G);
+FOUNDATION_EXPORT OMAppNetworkType const _Nonnull OMAppNetworkTypeUnknown;
+
+NS_SWIFT_NAME(AppNetworkTypeExportProtocol)
+@protocol OMAppNetworkTypeExport <NSObject, JSExport>
+
+@property (nonatomic, copy, readonly, nonnull) NSString *none;
+@property (nonatomic, copy, readonly, nonnull) NSString *WiFi;
+@property (nonatomic, copy, readonly, nonnull) NSString *WWan2G;
+@property (nonatomic, copy, readonly, nonnull) NSString *WWan3G;
+@property (nonatomic, copy, readonly, nonnull) NSString *WWan4G;
+@property (nonatomic, copy, readonly, nonnull) NSString *unknown;
+
+@end
+
+NS_SWIFT_NAME(AppNetworkTypeExport)
+@interface OMAppNetworkTypeExport : NSObject <OMAppNetworkTypeExport>
+@end
 
 
 NS_SWIFT_NAME(AppNetworkExportProtocol)
--- OMKit/Classes/AppExport/OMAppNetworkExport.m
+++ OMKit/Classes/AppExport/OMAppNetworkExport.m
@@ -13,7 +13,7 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _type = OMAppNetworkTypeUnkonwn;
+        _type = OMAppNetworkTypeUnknown;
     }
     return self;
 }
@@ -29,9 +29,39 @@
 @end
 
 
+@implementation OMAppNetworkTypeExport
+
+- (NSString *)none {
+    return  OMAppNetworkTypeNone;
+}
+
+- (NSString *)WiFi {
+    return OMAppNetworkTypeWiFi;
+}
+
+- (NSString *)WWan2G {
+    return OMAppNetworkType2G;
+}
+
+- (NSString *)WWan3G {
+    return  OMAppNetworkType3G;
+}
+
+- (NSString *)WWan4G {
+    return OMAppNetworkType4G;
+}
+
+- (NSString *)unknown {
+    return OMAppNetworkTypeUnknown;
+}
+
+@end
+
+
 OMAppNetworkType const _Nonnull OMAppNetworkTypeNone    = @"none";
 OMAppNetworkType const _Nonnull OMAppNetworkTypeWiFi    = @"WiFi";
 OMAppNetworkType const _Nonnull OMAppNetworkType2G      = @"2G";
 OMAppNetworkType const _Nonnull OMAppNetworkType3G      = @"3G";
 OMAppNetworkType const _Nonnull OMAppNetworkType4G      = @"4G";
-OMAppNetworkType const _Nonnull OMAppNetworkTypeUnkonwn = @"unknown";
+OMAppNetworkType const _Nonnull OMAppNetworkTypeUnknown = @"unknown";
+
